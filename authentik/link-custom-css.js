7ff0b0b7fa6d6f96f497b42bb6a3a794d478d00b
regbo <reggie.pierce@gmail.com>
2022-12-08 12:21:35 -0500

    Update link-custom-css.js

--- authentik/link-custom-css.js
+++ authentik/link-custom-css.js
@@ -1,11 +1,13 @@
-const appendCustomCSS(el) {
+const appendCustomCSS = (el) => {
+  console.log("appending css")
   var link = document.createElement('link');
+  link.rel = 'stylesheet'
   link.type = 'text/css';
   link.href = '/static/dist/custom.css';
   (el.head || el)
-  .appendChild(link);
+  .prepend(link);
 }
 
-(function(){
+(function() {
   addShadowRootListener(appendCustomCSS)
 })()
