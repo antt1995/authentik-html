4c253c47d91336a2bf0ab8a328b5e04d7533cf79
regbo <reggie.pierce@gmail.com>
2022-12-08 12:55:53 -0500

    Update link-custom-css.js

--- authentik/link-custom-css.js
+++ authentik/link-custom-css.js
@@ -1,15 +1,9 @@
-(function() {
-
-  let appendCustomCSS = (el) => {
-    console.log("appending css")
-    var link = document.createElement('link');
-    link.rel = 'stylesheet'
-    link.type = 'text/css';
-    link.href = '/static/dist/custom.css';
-    (el.head || el)
-    .prepend(link);
-  }
-
-  window.addShadowRootListener(appendCustomCSS)
-
-})()
+window.addShadowRootListener((el) => {
+  console.log("appending css")
+  var link = document.createElement('link');
+  link.rel = 'stylesheet'
+  link.type = 'text/css';
+  link.href = '/static/dist/custom.css';
+  (el.head || el)
+  .prepend(link);
+});
