d1334e5b11f0f7a9ae043fe7ffa584eb06c491fe
reggie pierce <reggie.pierce@gmail.com>
2022-12-11 20:57:28 -0500

    Update run-worker.sh

--- authentik/run-worker.sh
+++ authentik/run-worker.sh
@@ -30,8 +30,8 @@ AUTHENTIK_INJECT_JS_URLS=""
 while IFS='=' read -r -d '' NAME VALUE; do
     if [[ $NAME = AUTHENTIK_INJECT_URL* ]]; then
         if [[ $VALUE = *.css ]]; then
-            echo "injecting css:${VALUE}\n"
-            printf "\n\n//${VALUE}" >> $DIST_DIR/custom.css
+            echo "injecting css:${VALUE}"
+            printf "\n\n//${VALUE}\n\n" >> $DIST_DIR/custom.css
             curl -fsSL $VALUE >> $DIST_DIR/custom.css
         else
             echo "injecting js:${VALUE}"
